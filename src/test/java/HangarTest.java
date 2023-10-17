30d3be508021f27343b3bf99985bc201a388c02a
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-10-17 19:13:22 +0100

    Added 2 more vehicles in the canAddVehicle test in HangarTest.java.

--- src/test/java/HangarTest.java
+++ src/test/java/HangarTest.java
@@ -13,21 +13,28 @@ public class HangarTest {
     }
 
     @Test
-    void canGetName(){
+    void canGetName() {
         assertThat(hangar.getName()).isEqualTo("Hangar 1");
     }
 
     @Test
     void canCountVehicles() {
-    assertThat(hangar.countVehicles()).isEqualTo(0);
+        assertThat(hangar.countVehicles()).isEqualTo(0);
     }
 
     @Test
     void canAddVehicle() {
-        CruiseShip cruiseShip1 = new CruiseShip("Big one", 100_000, "Sea", 3_000, false);
+        CruiseShip Wonder_Of_The_Sea = new CruiseShip("Wonder of the Sea", 100_000, "Sea", 3_000, false);
 
-        hangar.addVehicle(cruiseShip1);
-        assertThat(hangar.countVehicles()).isEqualTo(1);
+        Helicopter Eurocopter_X3 = new Helicopter("Eurocopter X3", 100_000, "Air", 5, 18_500);
+
+        Bicycle The_Comet = new Bicycle("The Comet", 450, "Land", 2, 7);
+
+        hangar.addVehicle(Wonder_Of_The_Sea);
+        hangar.addVehicle(Eurocopter_X3);
+        hangar.addVehicle(The_Comet);
+
+        assertThat(hangar.countVehicles()).isEqualTo(3);
     }
 
 }
